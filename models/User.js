939ad81592760302f47bdf210885b11a068418be
Gray Smith <gray.smith00@gmail.com>
2018-09-07 16:25:04 -0600

    Change UserSchema to fit Twitter Oauth

--- models/User.js
+++ models/User.js
@@ -3,20 +3,57 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const UserSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true
-  },
   email: {
     type: String,
+    required: true,
     trim: true,
-    required: true
+    unique: true,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
   },
-  date: {
-    type: Date,
-    default: Date.now()
+  twitterProvider: {
+    type: {
+      id: String,
+      token: String
+    },
+    select: false
   }
 });
+
+UserSchema.statics.upsertTwitterUser = function(
+  token,
+  tokenSecret,
+  profile,
+  cb
+) {
+  let that = this;
+  return this.findOne(
+    {
+      'twitterProvider.id': profile.id
+    },
+    function(err, user) {
+      // no user was found, create a new one
+      if (!user) {
+        const newUser = new that({
+          email: profile.emails[0].value,
+          twitterProvider: {
+            id: profile.id,
+            token: token,
+            tokenSecret: tokenSecret
+          }
+        });
+
+        // save the new user
+        newUser.save(function(error, savedUser) {
+          if (error) {
+            console.log(error);
+          }
+          return cb(error, savedUser);
+        });
+      } else {
+        return cb(err, user);
+      }
+    }
+  );
+};
 
 module.exports = User = mongoose.model('User', UserSchema);
