ea197f50f26ae40c67e3fb0f2d5573fe8b5b5540
Gray Smith <gray.smith00@gmail.com>
2018-09-06 18:03:27 -0600

    Move production and development json config files into the config folder

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ if (app.get('env') === 'development') {
   debug('Morgan enabled...');
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
