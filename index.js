3e5aa60135be428e38344311bb110f4d80199990
Gray Smith <gray.smith00@gmail.com>
2018-09-08 09:28:17 -0600

    Change index to include passport twitter strategy config

--- index.js
+++ index.js
@@ -3,13 +3,44 @@ const app = express();
 
 const helmet = require('helmet');
 const morgan = require('morgan');
-const users = require('./routes/users');
-const home = require('./routes/home');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cors = require('cors');
+const session = require('express-session');
+const TwitterStrategy = require('passport-twitter').Strategy;
+
+const users = require('./routes/users');
+const home = require('./routes/home');
+const twitter = require('./routes/twitter');
+const CONSUMER_KEY = require('./keys').CONSUMER_KEY;
+const CONSUMER_SECRET = require('./keys').CONSUMER_SECRET;
+
+app.use(session({ resave: false, saveUninitialized: true, secret: 'SECRET' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Passport config
-const passportConfig = require('./config/passport');
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: CONSUMER_KEY,
+      consumerSecret: CONSUMER_SECRET,
+      callbackURL: 'http://localhost:5000/twitter/return',
+      includeEmail: true
+    },
+    function(token, tokenSecret, profile, callback) {
+      return callback(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, callback) => {
+  callback(null, user);
+});
+
+passport.deserializeUser((obj, callback) => {
+  callback(null, obj);
+});
 
 // DB config
 const db = require('./config/keys').mongoURI;
@@ -32,6 +63,17 @@ app.use(helmet());
 // Routes
 app.use('/api/users', users);
 app.use('/', home);
+app.use('/twitter', twitter);
 
+// CORS
+var corsOption = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  exposedHeaders: ['x-auth-token']
+};
+app.use(cors(corsOption));
+
+// Running the Server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
